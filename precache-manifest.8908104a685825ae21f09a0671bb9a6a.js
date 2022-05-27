937d2156c78c863e34802d1329c30807b954b0f3
shamsrupom94 <shams.rupom@gmail.com>
2022-05-28 00:14:18 +0200

    Updates

---
+++ precache-manifest.8908104a685825ae21f09a0671bb9a6a.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e96b6cd10c8b041eaf367b626c3be53",
+    "url": "/portfolio-shams/index.html"
+  },
+  {
+    "revision": "092bfd88b6e7025092ed",
+    "url": "/portfolio-shams/static/css/main.48dabe09.chunk.css"
+  },
+  {
+    "revision": "ed45b8541c77ff229731",
+    "url": "/portfolio-shams/static/js/2.09a3a7f2.chunk.js"
+  },
+  {
+    "revision": "71772b085033e92f89cdfb3071333458",
+    "url": "/portfolio-shams/static/js/2.09a3a7f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092bfd88b6e7025092ed",
+    "url": "/portfolio-shams/static/js/main.bedfcce8.chunk.js"
+  },
+  {
+    "revision": "5d1df183621576909e84",
+    "url": "/portfolio-shams/static/js/runtime-main.836bf386.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio-shams/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio-shams/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "1365ae8316be57b8e2c8ffa8cdc7e5f2",
+    "url": "/portfolio-shams/static/media/aiub.1365ae83.png"
+  },
+  {
+    "revision": "9b34735c80a69d44ae369697f9cd8d02",
+    "url": "/portfolio-shams/static/media/binate.9b34735c.png"
+  },
+  {
+    "revision": "347cb388efb6e5bd33fa6b604cb15923",
+    "url": "/portfolio-shams/static/media/contactMail.347cb388.png"
+  },
+  {
+    "revision": "3c769c9ab9276fd0cf87af8d30da83dc",
+    "url": "/portfolio-shams/static/media/eduhive.3c769c9a.png"
+  },
+  {
+    "revision": "2e7857cf4a65482b2d4a81dfccea5c4d",
+    "url": "/portfolio-shams/static/media/eduhiveapp.2e7857cf.png"
+  },
+  {
+    "revision": "1da029b3c1a275f00a75e7bb3ab8d9db",
+    "url": "/portfolio-shams/static/media/firebase.1da029b3.png"
+  },
+  {
+    "revision": "188371404d178409f8b689aa9c26a30b",
+    "url": "/portfolio-shams/static/media/ill_a.18837140.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio-shams/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "682b59a561d6f9a9243b981e3395c95a",
+    "url": "/portfolio-shams/static/media/pt.682b59a5.png"
+  },
+  {
+    "revision": "2c04a4f77ea225180f1877394ddfad6c",
+    "url": "/portfolio-shams/static/media/python.2c04a4f7.png"
+  },
+  {
+    "revision": "8251fe76d7487a28b07603515b300964",
+    "url": "/portfolio-shams/static/media/redux.8251fe76.png"
+  },
+  {
+    "revision": "5e20eb5b6520829739a396699c723ec0",
+    "url": "/portfolio-shams/static/media/scholars.5e20eb5b.png"
+  },
+  {
+    "revision": "c0a9409b2de7a03dbd40bd6cd3911c09",
+    "url": "/portfolio-shams/static/media/self.c0a9409b.jpg"
+  },
+  {
+    "revision": "ec1bf18592508764d0250cd160c692d7",
+    "url": "/portfolio-shams/static/media/ssl.ec1bf185.png"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/portfolio-shams/static/media/talksCardBack.de3da806.svg"
+  },
+  {
+    "revision": "c206ef655aa620a59d2ecffbb9aae646",
+    "url": "/portfolio-shams/static/media/texthive.c206ef65.png"
+  },
+  {
+    "revision": "5415b1a30925e87169fec0801e47840f",
+    "url": "/portfolio-shams/static/media/ts.5415b1a3.png"
+  },
+  {
+    "revision": "912cbb9e5b9e74976eb58ff7f4a59d5e",
+    "url": "/portfolio-shams/static/media/uds.912cbb9e.png"
+  }
+]);
